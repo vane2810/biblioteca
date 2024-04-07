d6a4f80b4e746612911621bee27e6586ba544200
vane2810 <olgavanessasf@gmail.com>
2024-04-07 00:30:13 -0600

    sem11

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('styles/about.css')}}">
+    <title>Acerca de nosotros</title>
+</head>
+<body>
+    <h3>Equipo</h3>
+    <ol>
+        <li>Hazel Brendaly Granados Ramos (SMSS041722)</li>
+        <li>Saúl Salomón Soto Machuca ()</li>
+        <li>Erick David Munguia Zelaya ()</li>
+        <li>Olga Vanessa Sorto Fuentes (SMSS055722)</li>
+    </ol>
+    <a href="/" class="btn">Inicio</a>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('styles/welcome.css')}}">
+    <title>Home</title>
+</head>
+<body>
+
+    <a href="/about" class="btn">Acerca de nosotros</a>
+</body>
+</html>
