cd3f4fbef7094b1aaa1a2103bf51c3dacb736899
Davidz920 <150879617+Davidz920@users.noreply.github.com>
2024-04-07 23:03:18 -0600

    sem11

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('styles/welcome.css')}}">
-    <title>Home</title>
+    <title>eLibroPlanet - Inicio</title>
+    <style>
+        /* colocar un estilo... */
+    </style>
 </head>
 <body>
+    <header>
+        <h1>Bienvenido a eLibroPlanet</h1>
+        <nav>
+            <ul>
+                <li><a href="#acerca">Acerca de nosotros</a></li>
+                <!--  podemos agregar más enlaces aquí para otras secciones importantes de la biblioteca -->
+            </ul>
+        </nav>
+    </header>
 
-    <a href="/about" class="btn">Acerca de nosotros</a>
+    <section id="acerca">
+        <h2>Acerca de nosotros</h2>
+        <p>
+            eLibroPlanet es una innovadora biblioteca digital que se destaca por ofrecer una extensa colección de recursos educativos en formato digital. Desde libros electrónicos hasta artículos académicos, pasando por planificaciones metodológicas, eLibroPlanet está comprometido con facilitar el acceso a conocimiento de calidad en todas las áreas del aprendizaje.
+
+Nuestra plataforma fue creada con la visión de transformar la manera en que se accede y se comparte el conocimiento en el ámbito educativo. Con eLibroPlanet, estudiantes, educadores y profesionales tienen la oportunidad de explorar un vasto repertorio de materiales educativos actualizados y relevantes para enriquecer su aprendizaje y enseñanza.
+        </p>
+        <!-- Aquí puedemos agregar más información sobre la plataforma -->
+    </section>
+
+    <!-- Podemos agregar más secciones importantes aquí -->
+
+    <footer>
+        <!-- podemos incluir información de contacto, enlaces adicionales, etc. -->
+    </footer>
 </body>
 </html>
+
+
