cf9bf67690337a6523780c370be2edda07a3accd
vane2810 <olgavanessasf@gmail.com>
2024-05-05 22:50:21 -0600

    sem15

--- app/Http/Controllers/CategoriasController.php
+++ app/Http/Controllers/CategoriasController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Categoria;
 
 class CategoriasController extends Controller
 {
-    //
+    
+public function index()
+{
+    $categories = Categoria::all();
+    return view('categories.index', compact('categories'));
+}
+
 }
---
+++ app/Models/Categoria.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'categories';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+}
